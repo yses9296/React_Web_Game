1e48d955cc5f3e8d385e69271e8d56b01c2a9522
Eunseo Choi <yses9296@gmail.com>
2022-10-20 15:53:32 +0900

    React 2-10

--- Lecture/components/WordRelay.jsx
+++ Lecture/components/WordRelay.jsx
@@ -1,19 +1,42 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
+const WordRelay = () => {
+    const [word, setWord] = useState('사자')
+    const [value, setValue] = useState('')
+    const [result, setResult] = useState('')
+    const inputRef = useRef(null);
 
+    const onChange = (e) => {
+        setValue(e.target.value);
+    }
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if(word[word.length -1] === value[0]){
+                setResult('Pass');
+                setWord(value);
+                setValue('');
+                inputRef.current.focus();
+        }
+        else {
+            setResult('Failed, Try again');
+            setValue('');
+            inputRef.current.focus();
         }
     }
-    render() {
-        return (
-            <div>This is WordRelay Component</div>
-        )
-    }
-}
 
+
+    return (
+        <div>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input type="text" placeholder='Enter the word' ref={inputRef} onChange={onChange} value={value} />
+                <button>Submit</button>
+            </form>
+            <div>{result}</div>
+        </div>
+    )
+    
+}
 
 module.exports = WordRelay;
