c48f367a9cebeb10146535594731ba25b1c3d0b8
Eunseo Choi <yses9296@gmail.com>
2022-10-20 16:48:59 +0900

    React 2-10

--- Lecture/webpack.config.js
+++ Lecture/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path'); //node 문법
+const webpack = require('webpack');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
     name: 'word-relay-setting',
@@ -19,15 +21,28 @@ module.exports = {
                 test: /\.jsx?$/,
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: [ '@babel/plugin-proposal-class-properties','react-refresh/babel'],
                 }
             }
         ]
     },
-
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+        new RefreshWebpackPlugin()
+    ],
     output: {
         path: path.join(__dirname, 'dist'),     //node 문법, 현재 폴더 위치 값 가져와서 dist 폴더,파일 위치 주소값 가져오기.
-        filename: 'app.js'                     //jsx파일을 모아 한 곳에 모아둘 파일
-    }//출력
-
+        filename: 'app.js',                     //jsx파일을 모아 한 곳에 모아둘 파일
+        publicPath: '/dist/',
+    },//출력
+    devServer: {
+        devMiddleware: {
+            publicPath: '/dist/'
+        },
+        static: {
+            directory: path.resolve(__dirname)
+        },
+        hot: true,
+    },
 }
