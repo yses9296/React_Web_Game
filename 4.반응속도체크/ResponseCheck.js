96072ed0d4490beb5499daa5cce78c723f9b27bd
Eunseo Choi <yses9296@gmail.com>
2022-10-25 19:50:28 +0900

    React 4-4

--- 4.반응속도체크/ResponseCheck.js
+++ 4.반응속도체크/ResponseCheck.js
@@ -1,13 +1,13 @@
-import React, { Component, useCallback, useState } from 'react'
+import React, { Component, useCallback, useRef, useState } from 'react'
 
 const ResponseCheck = () => {
     const [state ,setState] = useState('waiting');
     const [message, setMessage] = useState('Start with a Click');
     const [result, setResult] = useState([]);
+    const timeout = useRef(null);
+    const startTime = useRef();
+    const endTime = useRef();
 
-    let timeout;
-    let startTime;
-    let endTime;
 
     const onClickScreen = useCallback( (e) => {
         e.preventDefault();
@@ -15,31 +15,31 @@ const ResponseCheck = () => {
             setState('ready');
             setMessage('Click when the screen turns blue')
 
-            timeout = setTimeout( () => {
+            timeout.current = setTimeout( () => {
                 setState('now');
                 setMessage('Now Click!');
 
-                startTime = new Date();
+                startTime.current = new Date();
             }, Math.floor(Math.random() * 1000) + 2000); //2~3초 랜덤
 
         }
         else if (state === 'ready'){
-            clearTimeout(timeout) //now 상태가 나오지 않도록 초기화
+            clearTimeout(timeout.current) //now 상태가 나오지 않도록 초기화
             setState('waiting');
             setMessage("Don't be hasty");
         }
         else if (state === 'now'){
-            endTime = new Date();
+            endTime.current = new Date();
 
             setResult((prevResult)=>{
-                return [...prevResult, endTime - startTime]
+                return [...prevResult, endTime.current - startTime.current]
             })
             setState('waiting');
             setMessage("Start with a Click");
         }
     })
 
-    const reset = useCallback( () => {
+    const onReset = useCallback( () => {
         setResult([])
     })
     const renderAverage = useCallback( () => {
@@ -47,7 +47,7 @@ const ResponseCheck = () => {
             ? null 
             : <div>
                 <p>Average: {result.reduce((a,c) => a + c ) / result.length }ms</p>
-                <button onClick={reset}>Reset</button>
+                <button onClick={onReset}>Reset</button>
              </div>
     })
 
