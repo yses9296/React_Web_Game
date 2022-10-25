a342cb108d8691cce11c8503a3251afa4ca07a80
Eunseo Choi <yses9296@gmail.com>
2022-10-25 22:30:03 +0900

    React 5-2

--- 5.가위바위보/RockScissorsPaper.js
+++ 5.가위바위보/RockScissorsPaper.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
+//constructor(this.state) → render → (ref) → componentDidMount → 
+//(setState/props 바뀔 때) → shouldComponentUpdate(true) → render→ componentDidUpdate
+//부모가 요소를 없앴을 때 componentWillUnmount → 소멸
+
+const rspCoords = {
+    Rock: '0',
+    Scissors: '-142px',
+    Paper:'-284px'
+}
+
+const scores = {
+    Rock: 0,
+    Scissors: 1,
+    Paper: -1
+}
 
  class RockScissorsPaper extends Component {
     state = {
         result: '',
         score: 0,
-        imgCoord: 0
+        imgCoord: '0'
     }
-    componentDidMount(){ //render가 성공적으로 실행되고난 후 실해될 코드
+
+    interval;
+
+    componentDidMount(){ //render가 성공적으로 실행되고난 후 실해될 코드 >> 비동기 요청
+        this.interval = setInterval( () => {
+            if(this.state.imgCoord === rspCoords.Rock){
+                this.setState({
+                    imgCoord: rspCoords.Scissors
+                })
+            }
+            else if(this.state.imgCoord === rspCoords.Scissors){
+                this.setState({
+                    imgCoord: rspCoords.Paper
+                })
+            }
+            else if(this.state.imgCoord === rspCoords.Paper){
+                this.setState({
+                    imgCoord: rspCoords.Rock
+                })
+            }
+
+        }, 150);
 
     }
     componentDidUpdate(){ //리렌더링 후
 
     }
-    componentWillUnmount(){ //component가 제거되기 직전
+    componentWillUnmount(){ //component가 제거되기 직전 >> 비동기 요청
+        clearInterval(this.interval)
 
     }
 
