be8f45d92f2cd683dbdd7bb61fa9ae1e0d61fb4a
Eunseo Choi <yses9296@gmail.com>
2022-11-06 03:10:14 +0900

    React 7-5

--- 7.틱택토/components/Td.jsx
+++ 7.틱택토/components/Td.jsx
@@ -1,22 +1,25 @@
-import React, { useCallback } from 'react';
-import {CLICK_CELL, CHANGE_TURN} from './TicTacToe';
+import React, { useCallback, memo } from 'react';
+import { CLICK_CELL } from './TicTacToe';
 
-const Td = ({rowIndex, cellIndex, dispatch, cellData}) => {
+const Td = memo(({rowIndex, cellIndex, dispatch, cellData}) => {
+    console.log('td rendered');
 
     const onClickTd = useCallback(() => {
         console.log(rowIndex, cellIndex);
-        //이미 cell에 데이터가 존재한다면.
-        if(cellData){
+        
+        if(cellData){ //이미 cell에 데이터가 존재한다면.
             return;
         }
         dispatch( {type: CLICK_CELL, row: rowIndex, cell: cellIndex});
         // dispatch( {type: CHANGE_TURN})
     }, [cellData]);
-
+        // cellData가 바뀔 때마다 기억을해두었던 함수를 재실행.
+        // 왜 useCallback 함수 사용? >> onClick={onClickTd}> {cellData} 
+        // 함수를 props로 넣어주었을 때 불필요한 렌더링이 발생
     return (
         <td  onClick={onClickTd}> {cellData} </td>
     )
 
-}
+})
 
 export default Td;
