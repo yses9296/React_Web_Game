cf686043c605221706c8bcf9296d6fbba2bf9f64
Eunseo Choi <yses9296@gmail.com>
2022-11-05 20:20:35 +0900

    React 7-3

--- 7.틱택토/components/Table.jsx
+++ 7.틱택토/components/Table.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Tr from './Tr';
 
-const Table = ( {onClick, tableData} ) => {
+const Table = ( {onClick, tableData, dispatch} ) => {
     return (
-        <table onClick={onClick}>
-            {Array(tableData.length).fill().map( (tr, i) => (<Tr rowData={tableData[i]}></Tr>))}
+        <table>
+            {Array(tableData.length).fill().map( (tr, i) => (<Tr dispatch={dispatch} rowIndex={i} rowData={tableData[i]}></Tr>))}
             
         </table>
     )
--- 7.틱택토/components/TicTacToe.jsx
+++ 7.틱택토/components/TicTacToe.jsx
@@ -4,10 +4,16 @@ import Table from './Table';
 const initialState = {
   winner: '',
   turn: 'O',
-  tableData: [ ['', '', ''], ['', '', ''], ['', '', ''] ]
+  tableData: [ 
+    ['', '', ''], 
+    ['', '', ''], 
+    ['', '', ''] 
+  ]
 }
 
-const SET_WINNER = 'SET_WINNER';
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL'; 
+export const CHANGE_TURN = 'SET_TURN'; 
 
 const reducer = (state, action) => {
   switch(action.type){
@@ -18,6 +24,22 @@ const reducer = (state, action) => {
         ...state, 
         winner: action.winner
       }
+    case CLICK_CELL:{
+      const tableData = [...state.tableData]
+      tableData[action.row] = [...tableData[action.row]]; //immer라는 라이브러리로 가독성 해결 가능
+      tableData[action.row][action.cell] = state.turn;
+      
+      return {
+        ...state, 
+        tableData
+      }
+    }
+    case CHANGE_TURN: {
+      return {
+        ...state, 
+        turn: state.turn == 'O' ? 'X' : 'O'
+      }
+    }
   }
 }
 
@@ -33,7 +55,7 @@ const TicTacToe = () => {
 
   return (
     <div>
-      <Table onClick = {onClickTable} tableData={state.tableData}> </Table>
+      <Table onClick = {onClickTable} tableData={state.tableData} dispatch={dispatch}> </Table>
       {state.winner && <p>{state.winner} is Winner!</p> }
     </div>
   )
