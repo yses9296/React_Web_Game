6bf05f4f3e4a8767e33d3437183b48f69ddad2c0
Eunseo Choi <yses9296@gmail.com>
2022-11-06 20:35:13 +0900

    React 8-3

--- 8.지뢰찾기/components/Td.jsx
+++ 8.지뢰찾기/components/Td.jsx
@@ -1,12 +1,44 @@
 import React, { useContext } from 'react';
-import {TableContext} from './MineSearch'
+import {TableContext, CODE} from './MineSearch';
+
+
+const getTdStyle = (code) => {
+    switch(code){
+        case CODE.MINE:
+        case CODE.NORMAL:
+            return {
+                background: '#999'
+            }
+        case CODE.OPENED:
+            return {
+                background: '#fff'
+            }
+        default:
+            return {
+                background: '#fff'
+            }
+    }
+}
+const getTdText = (code) => {
+    switch(code){
+        case CODE.NORMAL:
+            return '';
+        case CODE.MINE:
+            return 'X';
+        default:
+            return '';
+    }
+}
+
 
 const Td = ({rowIndex, cellIndex}) => {
 
     const { tableData } = useContext(TableContext);
 
     return (
-        <td>{tableData[rowIndex][cellIndex]}</td>
+        <td style = {getTdStyle(tableData[rowIndex][cellIndex])}>
+            {getTdText(tableData[rowIndex][cellIndex])}
+        </td>
     );
 
 }
